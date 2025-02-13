bb5b9dff3b86c563ebb9b71b94a9ff2a14fabb96
pedroGongo <124210113+pedroGongo@users.noreply.github.com>
2025-02-13 13:24:50 +0000

    Update home.py

--- home.py
+++ home.py
@@ -68,7 +68,7 @@ def decode_firebase_token(id_token):
 
 #Função para gerar a URL de autenticação do Google
 def get_google_auth_url():
-    auth_url = f"https://accounts.google.com/o/oauth2/auth?client_id=1069155836636-ekve034mfjluv4s6icus6kb7bi3ltpnr.apps.googleusercontent.com&redirect_uri=https://listaa.streamlit.app&response_type=code&scope=email profile&prompt=select_account"
+    auth_url = f"https://accounts.google.com/o/oauth2/auth?client_id=1069155836636-ekve034mfjluv4s6icus6kb7bi3ltpnr.apps.googleusercontent.com&redirect_uri=https://listaa.streamlit.app/&response_type=code&scope=email profile&prompt=select_account"
     return auth_url
 
 
@@ -79,7 +79,7 @@ def exchange_code_for_token(code):
         "code": code,
         "client_id": "1069155836636-ekve034mfjluv4s6icus6kb7bi3ltpnr.apps.googleusercontent.com",
         "client_secret": "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",
-        "redirect_uri": "https://listaa.streamlit.app",
+        "redirect_uri": "https://listaa.streamlit.app/",
         "grant_type": "authorization_code",
     
     }
